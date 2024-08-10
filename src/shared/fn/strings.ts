2ad1ff291d6293e7e33a272a550b860bdbfacb74
Octavian_Jr <sirghi1979@mail.ru>
2024-08-10 12:52:16 +0300

    релизная версия с папкой build

--- src/shared/fn/strings.ts
+++ src/shared/fn/strings.ts
@@ -25,30 +25,6 @@ export default function getIndexFirstDiffLetter(
 
 /**
  *
- * @param controlText контрольная строка
- * @param wordIndex индекс слова
- * @returns индекс первого символа слова в контрольной строке иначе -1
- */
-export function getStartIndexWordIntoString(
-    controlText: string,
-    wordIndex: number
-): number {
-    let resultStartIndex = 0;
-
-    let startWordIndex = controlText.split(" ").findIndex((word, index) => {
-        if (index === wordIndex) {
-            resultStartIndex += index;
-            return true;
-        }
-        resultStartIndex += word.length + 1;
-        return false;
-    });
-
-    return startWordIndex === -1 ? -1 : resultStartIndex;
-}
-
-/**
- *
  * @param text текст
  * @param indentWords количество слова для отступа
  * @returns вырезает переданное количество слов и возвращает остаток
